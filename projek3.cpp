f1c50a99067ba1df166f8a06315dbe98bb8f389f
rommel22 <66980235+rommel22@users.noreply.github.com>
2021-05-18 02:01:09 +0700

    Add files via upload

--- projek3.cpp
+++ projek3.cpp
@@ -30,16 +30,16 @@ void createElementStack(nodePtr &newStack)
     std::string alamat;
     int umur;
     int noWork;
-    // std::cout << "Masukkan data pasien " << std::endl;
-    // std::cout << "Nomor KTP : " << std::endl;
+    std::cout << "Masukkan data pasien " << std::endl;
+    std::cout << "Nomor KTP : " << std::endl;
     std::cin >> noKtp;
-    // std::cout << "Nama : " << std::endl;
+    std::cout << "Nama : " << std::endl;
     std::cin >> nama;
-    // std::cout << "alamat : " << std::endl;
+    std::cout << "alamat : " << std::endl;
     std::cin >> alamat;
-    // std::cout << "Umur : " << std::endl;
+    std::cout << "Umur : " << std::endl;
     std::cin >> umur;
-    // std::cout << "Jenis Pekerjaan : " << std::endl;
+    std::cout << "Jenis Pekerjaan : " << std::endl;
     std::cin >> noWork;
     newStack = new node;
     newStack->noKtp = noKtp;
@@ -170,12 +170,6 @@ void createQElement(nodePtr &newQ, int priority)
     std::string alamat;
     int umur;
     int noWork;
-    std::cout << "Masukkan data pasien " << std::endl;
-    std::cin >> noKtp;
-    std::cin >> nama;
-    std::cin >> alamat;
-    std::cin >> umur;
-    std::cin >> noWork;
     newQ = new node;
     newQ->noKtp = noKtp;
     newQ->nama = nama;
@@ -298,32 +292,6 @@ void SearchList(char key, nodePtr &search, nodePtr head)
     }
 }
 
-void DeleteList(nodePtr head, char key)
-{
-    nodePtr search;
-    SearchList(key, search, head);
-    nodePtr delPtr = NULL;
-    delPtr = search;
-    if (delPtr == head)
-    {
-        delPtr = head;
-        delPtr->next = head;
-        delete delPtr;
-    }
-    else if (delPtr->next == NULL)
-    {
-        delPtr->prev = NULL;
-        delete delPtr;
-    }
-    else
-    {
-        delPtr->prev->next = delPtr->next;
-        delPtr->prev = NULL;
-        delPtr->next->prev = delPtr->prev;
-        delPtr->next = NULL;
-    }
-}
-
 void PrintList(list n)
 {
     nodePtr head = n.head;
@@ -331,41 +299,5 @@ void PrintList(list n)
     {
         std::cout << head->noKtp << " " << head->nama << " " << head->alamat << " " << head->umur << " " << head->noWork << " " << std::endl;
         head = head->next;
-    }
-}
-
-void Replace(int index, char noKtp, std::string nama, std::string alamat, char umur, int noWork, nodePtr n)
-{
-    nodePtr head,curr;
-    if (head == nullptr)
-    {
-        return;
-    }
-    else
-    {
-        curr = head;
-        if (index == 1)
-        {
-            head->noKtp = noKtp;
-            head->nama = nama;
-            head->umur = umur;
-            head->noWork = noWork;
-        }
-        else
-        {
-            for (int i = 0; i < index - 1; i++)
-            {
-                if (curr->next == nullptr)
-                {
-                    std::cout << "\nIndex tidak ditemukan";
-                    return;
-                }
-                curr = curr->next;
-            }
-            curr->noKtp = noKtp;
-            curr->nama = nama;
-            curr->umur = umur;
-            curr->noWork = noWork;
-        }
     }
 }
